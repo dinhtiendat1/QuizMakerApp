04c6e9432cbc9a54fcbe45688e8457c9ab9c2db7
dinhtiendat1 <73886259+dinhtiendat1@users.noreply.github.com>
2021-07-17 00:28:43 +0700

    cross orgin setting

--- src/main/java/com/huflit/controller/ExamController.java
+++ src/main/java/com/huflit/controller/ExamController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.huflit.service.QuestionService;
 @RestController
 @Scope("prototype")
 @RequestMapping("api/exams")
+@CrossOrigin
 public class ExamController {
 
 	@Autowired
